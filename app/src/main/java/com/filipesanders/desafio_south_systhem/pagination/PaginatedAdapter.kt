6bafed0a0d3ecaa82204f424d38fb09e34e9bf33
Filipe Sanders <54284424+filipefsanders@users.noreply.github.com>
2020-10-11 10:50:26 -0300

    Adicionadas classes de paginação

---
+++ app/src/main/java/com/filipesanders/desafio_south_systhem/pagination/PaginatedAdapter.kt
@@ -0,0 +1,154 @@
+package com.filipesanders.desafio_south_systhem.pagination
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.*
+import com.filipesanders.desafio_south_systhem.R
+
+
+abstract class PaginatedAdapter<T, VH : RecyclerView.ViewHolder>(
+    private val context: Context,
+    diffUtil: DiffUtil.ItemCallback<T>,
+    private val paginateOnTop: Boolean = false
+) : ListAdapter<T, RecyclerView.ViewHolder>(diffUtil) {
+
+    protected val CELL_TYPE_CONTENT = 0
+    protected val CELL_TYPE_LOAD = 1
+
+    var dataSource: PaginatedDataSource<*>? = null
+    private var layoutManager: RecyclerView.LayoutManager? = null
+
+    private val shouldLoadMore: Boolean
+        get() = dataSource?.value?.isLastPage == false
+
+    private val isRequestingData: Boolean
+        get() = dataSource?.isRequestingData?.value == true
+
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (getItemViewType(position) != CELL_TYPE_LOAD) {
+            onBindContentViewHolder(holder as VH, position)
+        }
+    }
+
+    override fun onBindViewHolder(
+        holder: RecyclerView.ViewHolder,
+        position: Int,
+        payloads: MutableList<Any>
+    ) {
+        if (getItemViewType(position) != CELL_TYPE_LOAD) {
+            onBindContentViewHolder(holder as VH, position, payloads)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType != CELL_TYPE_LOAD) {
+            onCreateContentViewHolder(parent, viewType)
+        } else {
+            LoaderViewHolder(
+                LayoutInflater.from(context).inflate(
+                    R.layout.cell_loader,
+                    parent,
+                    false
+                )
+            )
+        }
+    }
+
+    abstract fun onBindContentViewHolder(holder: VH, position: Int)
+    abstract fun onCreateContentViewHolder(parent: ViewGroup, viewType: Int): VH
+
+    open fun onBindContentViewHolder(holder: VH, position: Int, payloads: MutableList<Any>) {
+        super.onBindViewHolder(holder, position, payloads)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val loadPos = if (paginateOnTop) 0 else itemCount - 1
+        return if (shouldLoadMore && position == loadPos) {
+            CELL_TYPE_LOAD
+        } else {
+            CELL_TYPE_CONTENT
+        }
+    }
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        layoutManager = recyclerView.layoutManager
+        if (layoutManager != null) {
+            if (paginateOnTop) {
+                recyclerView.addOnScrollListener(onScrollListenerTop)
+            } else {
+                recyclerView.addOnScrollListener(onScrollListenerBottom)
+            }
+        }
+    }
+
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        if (paginateOnTop) {
+            recyclerView.removeOnScrollListener(onScrollListenerTop)
+        } else {
+            recyclerView.removeOnScrollListener(onScrollListenerBottom)
+        }
+        layoutManager = null
+        super.onDetachedFromRecyclerView(recyclerView)
+    }
+
+    private class LoaderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    private val onScrollListenerBottom = object : RecyclerView.OnScrollListener() {
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            var lastVisibleItem = 0
+
+            if (layoutManager is GridLayoutManager) {
+                lastVisibleItem = (layoutManager as GridLayoutManager).findLastVisibleItemPosition()
+            } else if (layoutManager is LinearLayoutManager) {
+                lastVisibleItem =
+                    (layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
+            }
+
+//            Log.v("Last", lastVisibleItem.toString())
+
+            if (!isRequestingData && lastVisibleItem == itemCount - 1) {
+
+                //Se chegou no fim da lista
+                if (shouldLoadMore) {
+                    dataSource?.loadMoreItems()
+                }
+            }
+        }
+    }
+
+    private val onScrollListenerTop = object : RecyclerView.OnScrollListener() {
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            var firstVisibleItem = 0
+
+            if (layoutManager is GridLayoutManager) {
+                firstVisibleItem =
+                    (layoutManager as GridLayoutManager).findFirstVisibleItemPosition()
+            } else if (layoutManager is LinearLayoutManager) {
+                firstVisibleItem =
+                    (layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+            }
+
+            Log.v("First", firstVisibleItem.toString())
+
+            if (!isRequestingData && firstVisibleItem == 0) {
+
+                //Se chegou no fim da lista
+                if (shouldLoadMore) {
+                    dataSource?.loadMoreItems()
+                }
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/filipesanders/desafio_south_systhem/pagination/PaginatedDataSource.kt
@@ -0,0 +1,96 @@
+package com.filipesanders.desafio_south_systhem.pagination
+
+import androidx.lifecycle.MutableLiveData
+import com.filipesanders.desafio_south_systhem.services.ServiceResponse
+import java.util.*
+
+abstract class PaginatedDataSource<T> : MutableLiveData<PaginatedDataSource.PaginatedData<T>>() {
+
+    data class PaginatedData<T>(
+        val items: List<T>? = null,
+        val error: ServiceResponse<*>? = null,
+        val isLastPage: Boolean = true,
+        val isLoadingInitial: Boolean = false
+    )
+
+    var isRequestingData = MutableLiveData(false)
+
+    private var reachedLastPage: Boolean = false
+
+    protected abstract fun loadFromServer(
+        offset: Int,
+        onDataLoaded: (data: PaginatedData<T>) -> Unit
+    )
+
+    //TODO: Melhoria: o load initial deveria ter prioridade ao o load normal.
+    // Talvez usar um backed live data ou algo assim.
+
+    fun loadInitialItems(silentRefresh: Boolean = false) {
+
+        if (isRequestingData.value == true)
+            return
+
+        isRequestingData.value = true
+
+        if(!silentRefresh) {
+            postValue(PaginatedData(isLoadingInitial = true))
+        }
+
+        loadFromServer(0) { response ->
+
+            reachedLastPage = response.isLastPage
+
+            val mergedList = response.items?.let {
+                appendReceivedItems(it, null)
+            }
+
+            val newServiceResponse = response.copy(
+                items = mergedList,
+                error = if (!reachedLastPage) response.error else null
+            )
+
+            isRequestingData.value = false
+
+            postValue(newServiceResponse)
+        }
+    }
+
+
+    fun loadMoreItems() {
+
+        if (isRequestingData.value == true || reachedLastPage)
+            return
+
+        isRequestingData.value = true
+
+        loadFromServer(value?.items?.size ?: 0) { response ->
+
+            reachedLastPage = response.isLastPage
+
+            val mergedList = response.items?.let {
+                appendReceivedItems(it, value?.items)
+            }
+
+            val newServiceResponse = response.copy(
+                items = mergedList,
+                error = if (!reachedLastPage) response.error else null
+            )
+
+            isRequestingData.value = false
+
+            postValue(newServiceResponse)
+        }
+    }
+
+    /**
+     * Faz a juncao da lista recebida da paginacao com os resultados existentes.
+     * Normalmente, faz apenas um addAll
+     */
+    protected open fun appendReceivedItems(
+        receivedItems: List<T>,
+        currentItems: List<T>?
+    ): List<T> {
+        return (currentItems ?: LinkedList()) + receivedItems
+    }
+
+}
